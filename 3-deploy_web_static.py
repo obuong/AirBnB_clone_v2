7daf19ba1fae723848b5fc49b61efaad75137205
obuong <iobuong2021@gmail.com>
2023-04-06 19:14:51 +0000

    Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.73.0.174", "35.196.78.105"]
+env.hosts = ["52.3.247.122", "52.72.28.213"]
 """The list of host server IP addresses."""
 
 
