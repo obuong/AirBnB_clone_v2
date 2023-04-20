6e404e3cad83c54e7de6dff2be7bd7471ceb6bdf
Esau Obuong <iobuong2021@gmail.com>
2023-04-20 05:03:55 -0700

    a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a flask script to display “Hello HBNB!”"""
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def index():
+    """a method to return hello HBNB"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
