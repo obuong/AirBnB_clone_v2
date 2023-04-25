6257f775df405aa1fbd191b6dbed71ceb00e0449
obuong <iobuong@gmail.com>
2023-04-25 10:21:33 +0300

    updated with the f string

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,20 +3,25 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def index():
     """a function to return hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
     """a function to return HBNB"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def index_c(text):
     """a function to return C is fun"""
-    return f'C {text.replace("_", " ")}'
+    return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -24,15 +29,18 @@ def python_is_cool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def a_number(n):
     """displays only when a number is passed on the url"""
-    return f'{n} is a number'
+    return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def render_html(n):
     """renders html page with a variable"""
-    return render_template("5-number.html", p = n)
+    return render_template("5-number.html", p=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def render_html_variables(n):
@@ -41,8 +49,8 @@ def render_html_variables(n):
         is_odd_or_even = "even"
     else:
         is_odd_or_even = "odd"
-        
-    return render_template("6-number_odd_or_even.html", p = n, m = is_odd_or_even)
+
+    return render_template("6-number_odd_or_even.html", p=n, m=is_odd_or_even)
 
 
 if __name__ == "__main__":
